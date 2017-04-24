ac25d08b10c90b4637edfc767593a0fc3a234695
Alla Sydoriak <alochka12@ukr.net>
2017-04-24 21:30:09 +0300

    Changed UI and modified type of img

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,17 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class Controller {
@@ -17,27 +20,39 @@ public class Controller {
     private Button chooseImg;
 
     @FXML
-    private TextField k;
-
-    @FXML
     private ImageView sourseImg;
 
     @FXML
     private ImageView newImg;
 
+
     public void chooseImgAction(ActionEvent event) throws MalformedURLException {
+        BufferedImage inputImage;
+        BufferedImage outputImage;
         FileChooser fc = new FileChooser();
+
+        fc.setInitialDirectory(new File("D:\\University\\6\\Proccessing of multimedia\\3lab\\img"));
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("BMP Files", "*.bmp"));
+
         File selectedFile = fc.showOpenDialog(null);
-        fc.setInitialDirectory(new File());
 
-        String thumbURL = selectedFile.toURI().toURL().toString();
-        Image imgLoad = new Image(thumbURL);
-
-        if(selectedFile != null) {
-            sourseImg.setImage(imgLoad);
-        } else {
-            System.out.println("Неправильний тип файлу");
+        try {
+            ImageInputStream imageInputStream = ImageIO.createImageInputStream(selectedFile);
+            inputImage = ImageIO.read(imageInputStream);
+            //outputImage = inputImage;
+            sourseImg.setImage(SwingFXUtils.toFXImage(inputImage, null));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+
+
+
+
+
+
+    public void redImgAction(ActionEvent event) {
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,14 +8,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Контрастування");
+        primaryStage.setScene(new Scene(root, 602, 416));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
