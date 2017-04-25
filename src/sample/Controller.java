30ebb96fae362faea2eea45d4b9014cf8c3b3131
Alla Sydoriak <alochka12@ukr.net>
2017-04-25 12:14:07 +0300

    Working red/green/blue img

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,7 +9,10 @@ import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -25,11 +28,12 @@ public class Controller {
     @FXML
     private ImageView newImg;
 
+    private BufferedImage inputImage;
+//  private BufferedImage outputImage;
+    private FileChooser fc = new FileChooser();
+
 
     public void chooseImgAction(ActionEvent event) throws MalformedURLException {
-        BufferedImage inputImage;
-        BufferedImage outputImage;
-        FileChooser fc = new FileChooser();
 
         fc.setInitialDirectory(new File("D:\\University\\6\\Proccessing of multimedia\\3lab\\img"));
         fc.getExtensionFilters().addAll(
@@ -40,7 +44,7 @@ public class Controller {
         try {
             ImageInputStream imageInputStream = ImageIO.createImageInputStream(selectedFile);
             inputImage = ImageIO.read(imageInputStream);
-            //outputImage = inputImage;
+//            outputImage = inputImage;
             sourseImg.setImage(SwingFXUtils.toFXImage(inputImage, null));
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,11 +52,49 @@ public class Controller {
     }
 
 
+    public void redImgAction() {
+        BufferedImage outputImage = copy(inputImage);
+        for (int i = 0; i < outputImage.getWidth(); i++) {
+            for (int j = 0; j < outputImage.getHeight(); j++) {
+                Color color = new Color(inputImage.getRGB(i, j));
+                Color rColor = new Color(color.getRed(), 0, 0);
+                outputImage.setRGB(i, j, rColor.getRGB());
+            }
+        }
+        newImg.setImage(SwingFXUtils.toFXImage(outputImage, null));
 
+    }
 
+    public void greenImgAction() {
+        BufferedImage outputImage = copy(inputImage);
 
+        for (int i = 0; i < outputImage.getWidth(); i++) {
+            for (int j = 0; j < outputImage.getHeight(); j++) {
+                Color color = new Color(inputImage.getRGB(i, j));
+                Color rColor = new Color(0, color.getGreen(), 0);
+                outputImage.setRGB(i, j, rColor.getRGB());
+            }
+        }
+        newImg.setImage(SwingFXUtils.toFXImage(outputImage, null));
+    }
 
+    public void blueImgAction(ActionEvent event) {
+        BufferedImage outputImage = copy(inputImage);
 
-    public void redImgAction(ActionEvent event) {
+        for (int i = 0; i < outputImage.getWidth(); i++) {
+            for (int j = 0; j < outputImage.getHeight(); j++) {
+                Color color = new Color(inputImage.getRGB(i, j));
+                Color rColor = new Color(0, 0, color.getBlue());
+                outputImage.setRGB(i, j, rColor.getRGB());
+            }
+        }
+        newImg.setImage(SwingFXUtils.toFXImage(outputImage, null));
+    }
+
+    private BufferedImage copy(BufferedImage source) {
+        ColorModel cm = source.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = source.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
     }
 }
